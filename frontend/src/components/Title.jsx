9a503beaf8e06b7abfa496ea8499238639431344
Billa05 <workinguse5@gmail.com>
2024-08-20 23:35:42 +0530

    fixed title

--- frontend/src/components/Title.jsx
+++ frontend/src/components/Title.jsx
@@ -3,10 +3,10 @@ export default function Title() {
     <>
       <h1 class="mt-7 ml-4 mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
         An application for{" "}
-        <span class=" text-blue-600 dark:text-blue-500">
-          safer, smarter, and more
-        </span>{" "}
-        resilient urban living.
+        <span class=" text-blue-600 dark:text-blue-500">safer, smarter,</span>{" "}
+        and more {" "}
+        <span class=" text-blue-600 dark:text-blue-500">resilient</span>{" "}
+        urban living.
       </h1>
       <p class="ml-4 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">
         This project develops a web app that enhances urban safety by providing
