5a69a73d54a834ef2cbcd6b3c37dfe48ae02752f
Billa05 <workinguse5@gmail.com>
2024-09-16 11:41:43 +0530

    some fixes

--- src/app/actions/CItyMapData.js
+++ src/app/actions/CItyMapData.js
@@ -4,17 +4,15 @@ import client from "@/db";
 
 export async function CityData(City) {
   try {
-    const allData = await client.crime.findMany({
+    const data = await client.crime.findMany({
       where: {
         city: City,
+        crime_description: {
+          in: ["VANDALISM", "ARSON", "TRAFFIC VIOLATION", "VEHICLE-STOLEN", "BURGLARY", "SHOPLIFTING", "DOMESTIC VIOLENCE", "SEXUAL ASSAULT"]
+        }
       },
+      take: 100
     });
-
-    // Shuffle the array to get random records
-    const shuffledData = allData.sort(() => 0.5 - Math.random());
-
-    // Take the first 100 records from the shuffled array
-    const data = shuffledData.slice(0, 100);
 
     return data;
   } catch (e) {
--- src/components/EmojiMap.jsx
+++ src/components/EmojiMap.jsx
@@ -125,7 +125,7 @@ export default function EmojiMap({ City }) {
 
       const popup = olaMaps.addPopup({ offset: [0, -30], anchor: "bottom" })
         .setHTML(`
-          <div class="bg-white p-2 rounded shadow">
+          <div class="bg-black p-2 rounded shadow">
             <h3 class="font-bold">${item.crime_description}</h3>
             <p>${item.additional_info || ""}</p>
           </div>
