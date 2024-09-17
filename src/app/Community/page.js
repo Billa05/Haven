df8cdf997129cedd6c73acfb60dd3e7ad9466b26
Billa05 <workinguse5@gmail.com>
2024-09-17 15:20:32 +0530

    some changes

--- src/app/Community/page.js
+++ src/app/Community/page.js
@@ -1,11 +1,10 @@
 import Chat from "@/components/CommunityChat";
 
-
-
 export default function Home() {
   return (
     <>
-      <Chat/>
+      <h1>Community Report</h1>
+      <Chat />
     </>
   );
 }
