5cc15a0be5dd64541c0ee994d57d8ddef93a172e
Billa05 <workinguse5@gmail.com>
2024-09-17 14:49:52 +0530

    eslint error fix

--- src/components/ReportCard.jsx
+++ src/components/ReportCard.jsx
@@ -21,7 +21,7 @@ export function ReportCard({label}) {
         <DialogHeader>
           <DialogTitle>Title</DialogTitle>
           <DialogDescription>
-            Make changes to your profile here. Click save when you're done.
+            Make changes to your profile here. Click save when youre done.
           </DialogDescription>
         </DialogHeader>
         <div className="grid gap-4 py-4">
